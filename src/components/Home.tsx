28517cd039727483e73ec38e4ed8d52c583596c4
Jamal <mohamed.eldeen.399@gmail.com>
2021-05-31 02:47:23 +0200

    feat: Implement log out logic

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,8 @@ import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router";
 
 import Questions from "./Questions";
 import NewQuestion from "./NewQuestion";
@@ -21,7 +23,13 @@ const CurrentUser = styled.div`
   padding: 5px;
 `;
 
+const StyledButton = styled(Button)`
+  display: block;
+  margin-top: 10px;
+`;
+
 const Home = () => {
+  const { push } = useHistory();
   const loggedInUser = useState<User>(
     JSON.parse(localStorage.getItem("loggedInUser") as string)
   );
@@ -50,11 +58,22 @@ const Home = () => {
       <CurrentUser>
         <p>Current User {loggedInUser[0].name}</p>
         <img
+          style={{}}
           height="50px"
           alt="userAvatar"
           // src={process.env.PUBLIC_URL + "/images/boy1.png"}
           src={`${process.env.PUBLIC_URL}/images/${loggedInUser[0].avatarURL}.png`}
         ></img>
+        <StyledButton
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            localStorage.clear();
+            push("/login");
+          }}
+        >
+          Log out
+        </StyledButton>
       </CurrentUser>
       {value === 0 && <Questions />}
       {value === 1 && <NewQuestion />}
